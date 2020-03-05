0d1cdac23f4749048c72cbe63fdb020261586aad
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-03-05 17:54:47 +0200

    Updates

---
+++ precache-manifest.24ecff4e062a33281c9e96009d9441b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4f2e89ca9b1f99aefa67590fe1e1b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "009e842cdc5d0845faf3",
+    "url": "./static/css/main.07cb65f7.chunk.css"
+  },
+  {
+    "revision": "3275c2ccf7840a9dbf02",
+    "url": "./static/js/2.209a0d1d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.209a0d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009e842cdc5d0845faf3",
+    "url": "./static/js/main.abc8bc78.chunk.js"
+  },
+  {
+    "revision": "acb15a54c7276c556e90",
+    "url": "./static/js/runtime-main.8f6a98c8.js"
+  }
+]);
